ffa67dd4dd2df3e42e3bd570706b628f307cc0d1
9min0 <75103452+9min0@users.noreply.github.com>
2022-02-14 12:29:54 +0900

    Feat: 추가 Movie구조체 생성자
    영화 검색 결과를 사용하기 위해 Movie 구조체에 생성자를 추가한다.

--- Movie/Movie/Model/Movie.swift
+++ Movie/Movie/Model/Movie.swift
@@ -13,4 +13,22 @@ struct Movie {
     let imageUrl: String?
     let director, actor: String?
     let userRating: String?
+    
+    init() {
+        self.title = ""
+        self.link = ""
+        self.imageUrl = ""
+        self.director = ""
+        self.actor = ""
+        self.userRating = ""
+    }
+    
+    init(title: String?, link: String?, imageUrl: String?, director: String?, actor: String?, userRating: String?) {
+        self.title = title
+        self.link = link
+        self.imageUrl = imageUrl
+        self.director = director
+        self.actor = actor
+        self.userRating = userRating
+    }
 }
