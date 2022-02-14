a8f3247e573b10b670779a0538ff0d37d42ca15a
9min0 <75103452+9min0@users.noreply.github.com>
2022-02-15 07:50:20 +0900

    Feat: 추가 MovieInfo View
    영화 정보를 표시하는 UIView 파일이 빠져 있었음. 해당 파일을 추가한다.

---
+++ Movie/Movie/View/MovieInfo.swift
@@ -0,0 +1,23 @@
+//
+//  MovieInfo.swift
+//  Movie
+//
+//  Created by KSNetDev1 on 2022/02/15.
+//
+
+import UIKit
+
+class MovieInfo: UIView {
+    @IBOutlet weak var directorLabel: UILabel!
+    @IBOutlet weak var actorLabel: UILabel!
+    @IBOutlet weak var userRatingLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var bookmarkBtn: UIButton!
+    
+    func update(with movie: Movie) {
+        self.directorLabel.text = "감독: " + (movie.director ?? "")
+        self.actorLabel.text = "배우: " + (movie.actor ?? "")
+        self.userRatingLabel.text = "평점: " + (movie.userRating ?? "")
+        self.thumbnailImageView.setImage(fromUrl: movie.imageUrl ?? "")
+    }
+}
