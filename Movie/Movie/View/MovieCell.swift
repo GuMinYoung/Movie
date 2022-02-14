ef12fe3681e3bc0fe089ed9f25b860b0a5a512d5
9min0 <75103452+9min0@users.noreply.github.com>
2022-02-14 12:41:24 +0900

    Feat: 추가 cell 업데이트 메서드
    테이블뷰셀에 데이터를 출력하기 위해 업데이트 메서드를 작성한다.

--- Movie/Movie/View/MovieCell.swift
+++ Movie/Movie/View/MovieCell.swift
@@ -26,8 +26,11 @@ class MovieCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
-    func update() {
-        
+    func update(_ movie: Movie) {
+        self.titleLabel.text = movie.title
+        self.directorLabel.text = "감독: " + (movie.director ?? "")
+        self.actorLabel.text = "배우: " + (movie.actor ?? "")
+        self.userRatingLabel.text = "평점: " + (movie.userRating ?? "")
+        self.thumbnailImageView.setImage(fromUrl: movie.imageUrl ?? "")
     }
-
 }
