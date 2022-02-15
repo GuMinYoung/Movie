c2e80d4a2057e0084cfb19bb4ce5e92be5732760
9min0 <75103452+9min0@users.noreply.github.com>
2022-02-15 14:09:37 +0900

    Feat: 변경 검색 응답데이터 구성
    nil인 데이터는 검색 결과 조회 및 북마크 등록 시 사용할 수 없으므로 응답데이터를 배열화 할 때 사용하는 함수를 map -> compactMap으로 변경한다.

--- Movie/Movie/ViewController/SearchViewController.swift
+++ Movie/Movie/ViewController/SearchViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Foundation
 import Alamofire
+import RealmSwift
 
 class SearchViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
@@ -29,6 +30,7 @@ class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         tableView.dataSource = self
         tableView.delegate = self
         // Do any additional setup after loading the view.
@@ -44,15 +46,15 @@ class SearchViewController: UIViewController {
         guard let keywords = textField.text else {return}
         SearchService.shared.search(keywords: keywords) { response in
             guard let searchResult = response.items else {return}
-            let movies = searchResult.map {
+            let movies = searchResult.compactMap {
                 return Movie(title: $0.title?.replacingOccurrences(of: "</b>", with: "")
-                                             .replacingOccurrences(of: "<b>", with: ""),
-                             link: $0.link,
+                                .replacingOccurrences(of: "<b>", with: ""),
+                             link: $0.link ?? "",
                              imageUrl: $0.image,
                              director: $0.director?.dropLast()
-                                                   .replacingOccurrences(of: "|", with: ", "),
+                                .replacingOccurrences(of: "|", with: ", "),
                              actor: $0.actor?.dropLast()
-                                             .replacingOccurrences(of: "|", with: ", "),
+                                .replacingOccurrences(of: "|", with: ", "),
                              userRating: $0.userRating)
             }
             self.viewModel?.movies = movies
