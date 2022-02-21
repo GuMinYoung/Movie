7032402ca0bf336036ca57d488b9f40f0d272b4a
9min0 <75103452+9min0@users.noreply.github.com>
2022-02-21 15:52:39 +0900

    Feat: 변경 북마크 저장 방식
    - 기존 코드에서는 북마크 등록된 영화를 각각의 object로 저장소에 저장하고 link를 key로 사용해서 북마크 등록/해제했다. 그러나 북마크 화면에서 .objects로 저장소에 저장된 데이터를 가져올 때 이슈가 발생했다. print, dump 함수로 출력할 때는 데이터가 들어있는 것으로 보이나, 데이터를 사용하려 하면 property가 비어있었다.
    - 수정한 코드에서는 북마크 목록 자체를 저장소에 List로 저장한다.
    - 보다 Swift적인 문법으로 보이기 위해 Realm object 생성 시 @objc dynamic 키워드 대신 업데이트 된 @Persisted를 적용한다. 재정의한 primaryKey 메서드도 제거한다. key를 사용하지 않고 리스트 요소의 link값을 비교해 북마크 등록/해제를 진행하므로 @Persisted(primaryKey: true) 또한 사용하지 않는다.
    - 리스트의 요소는 RealmSwift.Object 타입이므로 Movie 구조체 인스턴스와의 형변환을 위해 필요한 Persistable 프로토콜의 메서드 및 생성자를 정의한다.

--- Movie/Movie/View/MovieCell.swift
+++ Movie/Movie/View/MovieCell.swift
@@ -28,10 +28,10 @@ class MovieCell: UITableViewCell {
     
     func update(_ movie: Movie) {
         self.titleLabel.text = movie.title
-        self.directorLabel.text = "감독: " + (movie.director ?? "")
-        self.actorLabel.text = "배우: " + (movie.actor ?? "")
-        self.userRatingLabel.text = "평점: " + (movie.userRating ?? "")
-        self.thumbnailImageView.setImage(fromUrl: movie.imageUrl ?? "")
+        self.directorLabel.text = "감독: " + (movie.director)
+        self.actorLabel.text = "배우: " + (movie.actor)
+        self.userRatingLabel.text = "평점: " + (movie.userRating)
+        self.thumbnailImageView.setImage(fromUrl: movie.imageUrl )
         self.setBookmarkBtnImage(status: movie.isBookmark)
     }
     
--- Movie/Movie/View/MovieInfo.swift
+++ Movie/Movie/View/MovieInfo.swift
@@ -15,9 +15,9 @@ class MovieInfo: UIView {
     @IBOutlet weak var bookmarkBtn: UIButton!
     
     func update(with movie: Movie) {
-        self.directorLabel.text = "감독: " + (movie.director ?? "")
-        self.actorLabel.text = "배우: " + (movie.actor ?? "")
-        self.userRatingLabel.text = "평점: " + (movie.userRating ?? "")
-        self.thumbnailImageView.setImage(fromUrl: movie.imageUrl ?? "")
+        self.directorLabel.text = "감독: " + (movie.director)
+        self.actorLabel.text = "배우: " + (movie.actor )
+        self.userRatingLabel.text = "평점: " + (movie.userRating)
+        self.thumbnailImageView.setImage(fromUrl: movie.imageUrl)
     }
 }
